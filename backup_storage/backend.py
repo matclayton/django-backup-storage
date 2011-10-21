7d287a97b46af905abef400f830fd359e7922031
Gun.io Whitespace Robot <contact@gun.io>
2011-10-21 11:50:35 -0400

    Remove whitespace [Gun.io WhitespaceBot]

--- backup_storage/backend.py
+++ backup_storage/backend.py
@@ -1,20 +1,20 @@
 from django.core.files.storage import get_storage_class, Storage
- 
+
 from backup_storage.tasks import CopyToStorage
 from django.utils.encoding import force_unicode
 
 import os
-  
+
 class QueuedBackupStorage(Storage):
     def __init__(self, local, remote):
         self.local_class = local
         self.local = get_storage_class(self.local_class)()
         self.remote_class = remote
         self.remote = get_storage_class(self.remote_class)()
-    
+
     def open(self, name, **kwargs):
-        return self.local.open(name, **kwargs)   
-        
+        return self.local.open(name, **kwargs)
+
     def save(self, name, content):
         """
         Saves new content to the file specified by name. The content should be a
@@ -32,10 +32,10 @@ class QueuedBackupStorage(Storage):
 
         # Store filenames with forward slashes, even on Windows
         return force_unicode(name.replace('\\', '/'))
-    
+
     def get_valid_name(self, name):
-        return self.local.get_valid_name(name)  
-    
+        return self.local.get_valid_name(name)
+
     def get_available_name(self, name):
         """
         Returns a filename that's free on both target storage systems, and
@@ -50,23 +50,23 @@ class QueuedBackupStorage(Storage):
             file_root += '_'
             # file_ext includes the dot.
             name = os.path.join(dir_name, file_root + file_ext)
-        return name 
-    
+        return name
+
     def path(self, name):
         return self.local.path(name)
-    
+
     def delete(self, name):
         self.local.delete(name)
         self.remote.delete(name)
-    
+
     def exists(self, name):
         return (self.local.exists(name) or self.remote.exists(name))
-    
+
     def listdir(self, name):
         return self.local.listdir(name)
-    
+
     def size(self, name):
         return self.local.size(name) if self.local.exists(name) else self.remote.size(name)
-    
+
     def url(self, name):
         return self.local.url(name) if self.local.exists(name) else self.remote.url(name)
